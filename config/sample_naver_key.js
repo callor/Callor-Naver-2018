832972f1bc8bf000544f06358df0a80a63f5d22d
callor <callor@callor.com>
2018-08-17 10:05:38 +0900

    naverKey

---
+++ config/sample_naver_key.js
@@ -0,0 +1,11 @@
+/**
+ * 실제 프로젝트를 실행할때는
+ * naver open ID와 secret 키를 발급받아
+ * 아래 항목을 수정하고
+ * 파일을 naver_key.js로 바꾸어 사용해야 한다.
+ */
+
+module.exports = {
+	CLIENT_ID : 'Naver Open ID',
+	CLIENT_SEC : 'Naver Open Secret'
+}
